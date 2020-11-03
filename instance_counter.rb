1dbb1c2464c0ca0f7d1dd971d6bffd8adabb367a
banpresto <sexyteemo@gmail.com>
2020-11-03 10:14:33 +0300

    lesson 4

--- instance_counter.rb
+++ instance_counter.rb
@@ -4,18 +4,27 @@ module InstanceCounter
     base.include InstanceMethod
   end
 
-  module ClassMethod 
-    attr_accessor :instances
+  module ClassMethod
+    attr_accessor :instance
+    #Подскажите я уверен существует другой метод как это реализовать, но не могу понять как.
+    def instances
+      if instance == nil
+        return 0
+      else
+        instance
+      end
+    end
   end
 
   module InstanceMethod
     private
     
     def register_instance
-      if self.class.instances == nil
-        self.class.instances = 1
+      if self.class.instance == nil
+        self.class.instance = 1
       else
-        self.class.instances += 1
+        self.class.instance += 1
+        puts self.class.instance
       end
     end
   end
